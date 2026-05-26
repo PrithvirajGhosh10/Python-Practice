3e99fab3f81b9156508dead8e74135ae139c3d19
PrithvirajGhosh10 <aloghosh802@gmail.com>
2026-05-26 08:31:02 +0530

    Update 03_problem3_inheritance.py

--- 03_problem3_inheritance.py
+++ 03_problem3_inheritance.py
@@ -1,5 +1,6 @@
-#3. Create a class 'Employee' and add salary and increment properties to it.
+#Create a class 'Employee' and add salary and increment properties to it.
 #Write a method 'salaryAfterIncrement' method with a @property decorator with a setter which changes the value of increment based on the salary.
+
 
 class Employee:
     salary=6484
