35ec31e71d51e0c7f978b52130a0174c9dc7e753
PrithvirajGhosh10 <aloghosh802@gmail.com>
2026-05-27 14:03:20 +0530

    Filter a list of numbers which are divisible by 5
    Write a program to filter a list of numbers which are divisible by 5.

--- 04_problem4.py
+++ 04_problem4.py
@@ -1,7 +1,8 @@
-# 4. A file contains a word "Donkey" multiple times. You need to write a program which replace this word with ##### by updating the same file.
-word="Donkey"
-with open("ch9-ps-p4.txt" , "r") as f:
-    content=f.read()
-contentNew=content.replace(word,"#####")
-with open("ch9-ps-p4.txt","w") as f:
-    f.write(contentNew)
+#4. Write a program to filter a list of numbers which are divisible by 5.
+l=[1,33,15,51,45,40,10,84,621,84]
+def divisible(n):
+    if(n%5==0):
+        return True
+    return False
+a=filter(divisible,l)
+print("NO. Divisible by 5: ",list(a))
