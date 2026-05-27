df17faf7b0c8aaa6d180fec3d78972a59f96248b
PrithvirajGhosh10 <aloghosh802@gmail.com>
2026-05-27 13:52:11 +0530

    Format using the format function
    . Write a program to input name, marks and phone number of a student and format it using the format function like below:
    
    #"The name of the student is Harry, his marks are 72 and phone number is 99999888"

---
+++ 02_problem2.py
@@ -0,0 +1,9 @@
+#2. Write a program to input name, marks and phone number of a student and format it using the format function like below:
+
+#"The name of the student is Harry, his marks are 72 and phone number is 99999888"
+
+name= input("Enter name: ")
+marks = int(input("Enter marks: "))
+phone = int(input("phone no.: "))
+b= " The name of the student is {0}, his marks are {1}, and phone number is {2}".format(name,marks,phone)
+print(b)
